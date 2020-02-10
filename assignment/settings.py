e45bafd91c9dd8ce373f062a8624491562dcc4ac
prafullsinha <h2so4007@gmail.com>
2020-02-10 12:57:24 +0530

    debug

--- assignment/settings.py
+++ assignment/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'g(2gem)p0jol=g(e))7z_f$hjk)44@-@28cm4=sy*15o3ufkbo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['notemanager2019.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['notemanager2019.herokuapp.com']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,7 +136,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#
-# prod_db = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
